c84bf99641bf50be148906bb11c30bba4f61a84c
IanAcosta7 <ianacosta700@gmail.com>
2021-02-15 22:33:39 -0300

    Added composer.

---
+++ server/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '17eaa96ee32cfb4fb4735c2ffa0dde45994e38fc',
+    'name' => 'courseit/lorem-ipsum',
+  ),
+  'versions' => 
+  array (
+    'courseit/lorem-ipsum' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '17eaa96ee32cfb4fb4735c2ffa0dde45994e38fc',
+    ),
+  ),
+);
